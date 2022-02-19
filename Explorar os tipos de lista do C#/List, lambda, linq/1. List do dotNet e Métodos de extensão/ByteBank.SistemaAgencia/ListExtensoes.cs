d6744872ec7a64f400fdf8954270a99abb6a6185
M1guelOl1veira <miguel.ao1086@gmail.com>
2022-02-19 07:42:31 -0300

    Estudos de ontem

---
+++ Explorar os tipos de lista do C#/List, lambda, linq/1. List do dotNet e Métodos de extensão/ByteBank.SistemaAgencia/ListExtensoes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public static class ListExtensoes
+    {
+        //Sintaxe do método de extensão, a classe precisa ser estática e é necessário o this no argumento.
+        public static void AdicionarVarios(this List<int> listaDeInteiros, params int[] itens)
+        {
+            foreach(int item in itens)
+            {
+                listaDeInteiros.Add(item);
+            }
+        }
+    }
+}
