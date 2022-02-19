95d44d83bece4faf9f47525d814b6b1fa4143f15
M1guelOl1veira <miguel.ao1086@gmail.com>
2022-02-19 07:31:05 -0300

    Estudos

---
+++ Aprofundar no C#/Strings, expressões regulares e classe Object/4. Expressões regulares, quantificadores, Classe Object, ToString, Equals/ByteBank.Modelos/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Profissao { get; set; }
+
+        //Toda classe é derivada de object implicitamente.
+        public override bool Equals(object obj)
+        {
+            //Cliente outroCliente = (Cliente)obj; - Outra maneira de conversão.
+            //É necessário converter obj para a classe cliente para acessar os parâmetros da classe cliente e depois compará-los.
+            //Essa maneira é chamada de cast.
+            Cliente outroCliente = obj as Cliente;
+
+            //Isso é feito pq caso o parâmetro passado não seja do tipo cliente a variável outroCliente irá ficar sem referência.
+            if (outroCliente == null)
+            {
+                return false;
+            }
+
+            //Podemos comparar todos os atributos ou apenas os identificadores, como feito abaixo.
+            return CPF == outroCliente.CPF;
+        }
+    }
+}
---
+++ teste/List, Lambda e Linq/1. Array - Valores padrões, Iterando, Length, Inicializar/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente[] contas = new ContaCorrente[]
+                {
+                    new ContaCorrente(874, 5679787),
+                    new ContaCorrente(874, 4456668),
+                    new ContaCorrente(874, 7781438)
+                };
+            
+            for(int indice = 0; indice < contas.Length; indice++)
+            {
+                ContaCorrente contaAtual = contas[indice];
+                Console.WriteLine($"Conta {indice} {contaAtual.Numero}");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void TestaArrayInt()
+        {
+            //Sintaxe de inicialização de um array.
+            //Valor padrão de um array é 0.
+            int[] idades = null;
+            idades = new int[] { 15, 28, 35, 50, 28 };
+                
+            Console.WriteLine(idades.Length);
+
+            int acumulador = 0;
+
+            //Iterando arrays;
+            for (int indice = 0; indice < idades.Length; indice++)
+            {
+                int idade = idades[indice];
+
+                Console.WriteLine($"Acessando o array idades no índice {indice}");
+                Console.WriteLine($"Valor de idades[{indice}] = {idade}");
+
+                acumulador += idade;
+            }
+
+            int media = acumulador / idades.Length;
+            Console.WriteLine($"Média de idades = {media}");
+        }
+
+    }
+}
